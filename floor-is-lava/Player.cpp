5f795d50761b02a27a9f98cefecca515de0768b2
iansinphi <32443446+iansinphi@users.noreply.github.com>
2018-04-23 14:35:27 -0500

    Update Player.cpp

--- floor-is-lava/Player.cpp
+++ floor-is-lava/Player.cpp
@@ -5,6 +5,7 @@
 Player::Player() {
 	// Player's speed
 	speed = 500;
+	gravity = 500.0f
 
 	// associate a texture with the sprite
 	texture.loadFromFile("player.png");
@@ -13,6 +14,8 @@ Player::Player() {
 	// set Player's starting position
 	position.x = 500;
 	position.y = 500;
+//	velocity.x 100;
+	velocity.y = 500;
 }
 
 
@@ -36,6 +39,14 @@ void Player::stopRight() {
 	rightIsPressed = false;
 }
 
+void Player::startJump() {
+	spaceIsPressed = true;
+}
+
+void Player::endJump() {
+	spaceIsPressed = false;
+}
+
 // move Player based on the input this frame,
 // ... the time elapsed, and the speed
 void Player::update(float timeDelta) {
@@ -45,6 +56,20 @@ void Player::update(float timeDelta) {
 
 	if (leftIsPressed) {
 		position.x -= speed * timeDelta;
+	}
+	
+	if (spaceIsPressed) {
+		velocity.y = -600.0f;
+		
+		position.y += velocity.y * timeDelta;
+		position.x += velocity.x * timeDelta;
+		velocity.y += gravity * timeDelta;
+	}
+	else if(position <= 500)
+	{
+		position.y += gravity * timeDelta;
+		if (velocity.y < -100.0f)
+			velocity.y = -100.0f;
 	}
 
 	// move Player sprite to new position
